452a7a1fb6870b66896c985580558985899a3452
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-04 16:07:42 -0500

    Use a dash

--- spec/phone_number_formatter_spec.rb
+++ spec/phone_number_formatter_spec.rb
@@ -2,13 +2,13 @@ require "spec_helper"
 
 describe PhoneNumberFormatter do
   it "formats phone numbers like (650) 123-1234" do
-    expect(PhoneNumberFormatter.new("650-123-1234").format).to eq "(650) 123 1234"
-    expect(PhoneNumberFormatter.new("650 123 1234").format).to eq "(650) 123 1234"
-    expect(PhoneNumberFormatter.new("650 123-1234").format).to eq "(650) 123 1234"
-    expect(PhoneNumberFormatter.new("650-123 1234").format).to eq "(650) 123 1234"
-    expect(PhoneNumberFormatter.new("(650) 123 1234").format).to eq "(650) 123 1234"
+    expect(PhoneNumberFormatter.new("650-123-1234").format).to eq "(650) 123-1234"
+    expect(PhoneNumberFormatter.new("650 123 1234").format).to eq "(650) 123-1234"
+    expect(PhoneNumberFormatter.new("650 123-1234").format).to eq "(650) 123-1234"
+    expect(PhoneNumberFormatter.new("650-123 1234").format).to eq "(650) 123-1234"
+    expect(PhoneNumberFormatter.new("(650) 123 1234").format).to eq "(650) 123-1234"
     expect(PhoneNumberFormatter.new("6501231234").format).to eq "(650) 123 1234"
-    expect(PhoneNumberFormatter.new("    6501231234    ").format).to eq "(650) 123 1234"
+    expect(PhoneNumberFormatter.new("    6501231234    ").format).to eq "(650) 123-1234"
   end
 
   it "passes badly-formatted numbers through unchanged" do
