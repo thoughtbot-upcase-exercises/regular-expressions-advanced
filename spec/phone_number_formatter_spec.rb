b322b8347e44f82effb35933c830870fd1d212e6
Gabe Berke-Williams <gabebw@gabebw.com>
2014-11-05 16:19:20 -0500

    Use separate specs for each test case
    Add a bonus spec.

--- spec/phone_number_formatter_spec.rb
+++ spec/phone_number_formatter_spec.rb
@@ -1,18 +1,44 @@
 require "spec_helper"
 
 describe PhoneNumberFormatter do
-  it "formats phone numbers like (650) 123-1234" do
-    expect(PhoneNumberFormatter.new("650-123-1234").format).to eq "(650) 123-1234"
-    expect(PhoneNumberFormatter.new("650 123 1234").format).to eq "(650) 123-1234"
-    expect(PhoneNumberFormatter.new("650 123-1234").format).to eq "(650) 123-1234"
-    expect(PhoneNumberFormatter.new("650-123 1234").format).to eq "(650) 123-1234"
-    expect(PhoneNumberFormatter.new("(650) 123 1234").format).to eq "(650) 123-1234"
-    expect(PhoneNumberFormatter.new("6501231234").format).to eq "(650) 123 1234"
-    expect(PhoneNumberFormatter.new("    6501231234    ").format).to eq "(650) 123-1234"
+  describe "successfully formatting a number to look like (650) 123-1234" do
+    it "formats a number like 650-123-1234" do
+      expect(PhoneNumberFormatter.new("650-123-1234").format_number).to eq "(650) 123-1234"
+    end
+
+    it "formats a number with spaces instead of dashes" do
+      expect(PhoneNumberFormatter.new("650 123 1234").format_number).to eq "(650) 123-1234"
+    end
+
+    it "formats a number with spaces and dashes" do
+      expect(PhoneNumberFormatter.new("650 123-1234").format_number).to eq "(650) 123-1234"
+      expect(PhoneNumberFormatter.new("650-123 1234").format_number).to eq "(650) 123-1234"
+    end
+
+    it "formats a number with parentheses and spaces" do
+      expect(PhoneNumberFormatter.new("(650) 123 1234").format_number).to eq "(650) 123-1234"
+    end
+
+    it "formats a number with no separators at all" do
+      expect(PhoneNumberFormatter.new("6501231234").format_number).to eq "(650) 123-1234"
+    end
+
+    it "formats a number with spaces at both ends" do
+      expect(PhoneNumberFormatter.new("    6501231234    ").format_number).to eq "(650) 123-1234"
+    end
+
+    it "formats a number separated by x's" do
+      expect(PhoneNumberFormatter.new("650x123x1234").format_number).to eq "(650) 123-1234"
+    end
   end
 
   it "passes badly-formatted numbers through unchanged" do
-    expect(PhoneNumberFormatter.new("650 123 123").format).to eq "650 123 123"
-    expect(PhoneNumberFormatter.new("650x123x1234").format).to eq "650x123x1234"
+    expect(PhoneNumberFormatter.new("650 123 123").format_number).to eq "650 123 123"
+  end
+
+  it "can handle numbers with international codes" do
+    pending "After getting all the other specs passing, remove this 'pending' line"
+
+    expect(PhoneNumberFormatter.new("+16501231234").format_number).to eq "+1 (650) 123-1234"
   end
 end
